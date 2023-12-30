615ada54521a846b25b0df957028b92a13d67073
Harole <47141290+HaroleDev@users.noreply.github.com>
2023-12-30 13:25:59 +0700

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -91,11 +91,11 @@ sudo apt install
 # Removing leftovers
 sudo snap remove --purge firefox
 sudo snap remove --purge snap-store
-sudo snap remove --purge gnome-3-38-2004
+sudo snap remove --purge gnome-42-2204
 sudo snap remove --purge gtk-common-themes
 sudo snap remove --purge snapd-desktop-integration
 sudo snap remove --purge bare
-sudo snap remove --purge core20
+sudo snap remove --purge core22
 sudo snap remove --purge snapd
 sudo rm -rf -v /var/cache/snapd/
 sudo apt autoremove --purge update-manager snapd firefox -y
